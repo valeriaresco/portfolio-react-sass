9ee389bdffc2c8061b01cdac7fb04d6969a0805e
Valeria Resco <valeriaresco99@gmail.com>
2022-11-22 21:12:58 -0300

    iniciando ajuste de cores

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -4,8 +4,8 @@ import '../styles/components/socialnetworks.sass'
 
 //array de objetos
 const socialNetworks = [
-    { name: "linkedin", icon: <FaLinkedinIn /> },
-    { name: "github", icon: <FaGithub /> },
+    { name: "linkedin", icon: <FaLinkedinIn />, link:'https://www.linkedin.com/in/valéria-resco' },
+    { name: "github", icon: <FaGithub />, link:'https://github.com/valeriaresco' },
     { name: "instagram", icon: <FaInstagram /> },
 ]
 
@@ -13,7 +13,7 @@ const SocialNetworks = () => {
     return (
     <section id="social-networks">
         {socialNetworks.map((network) => (
-            <a href="#" className="social-btn" id={network.name} key={network.name}>
+            <a href={network.link} className="social-btn" id={network.name} key={network.name}>
                 {network.icon}
             </a>
         ))}
