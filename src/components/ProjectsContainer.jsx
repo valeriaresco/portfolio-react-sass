8ebcd94a987adedca13c20ed65783d0cf5446fb4
Valeria Resco <valeriaresco99@gmail.com>
2022-11-30 21:51:42 -0300

    ajustando cores e layout

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -2,7 +2,7 @@
 const ProjectsContainer = () => {
     return <section className="projects-container">
         <h2>Projetos</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, fugiat. Itaque repellat optio ea consequuntur voluptas quam sunt quidem quaerat quisquam deserunt culpa nisi fuga, natus possimus, unde inventore vero.</p>
+        <p>Abaixo ficarão os meus próximos projetos :)</p>
 
         <a href="#" className="btn">
             Ver Projetos
